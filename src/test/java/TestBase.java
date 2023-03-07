68bc830b411a9b9eab3697e11cd2694b8607d65b
karolina b <karolinabargiel94@gmail.com>
2023-03-07 17:49:30 +0100

    First commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
